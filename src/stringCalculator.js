c3fb65d87697b41286bd23a4527e56bd3f8a0df9
sachinpsgg <sachin@psglobalgroup.com>
2025-06-12 19:06:22 +0530

    code refactoring

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,21 +1,52 @@
 function add(numbers) {
-    if (numbers === "") {
-        return 0;
-    }
-    let delimiters = [','];
-    if (numbers.startsWith('//')) {
-        const parts = numbers.split('\n');
-        const delimiterPart = parts[0].slice(2);
-        delimiters = delimiterPart.split(/\]\[|\[/).map(d => d.replace(/\[|\]/g, ''));
-        numbers = parts[1];
+    if (!numbers) return 0;
+
+    const { delimiters, numberString } = parseDelimiters(numbers);
+    const numArray = extractNumbers(numberString, delimiters);
+    validateNegatives(numArray);
+
+    return sumNumbers(numArray);
+}
+
+function parseDelimiters(numbers) {
+    const defaultDelimiters = [','];
+    if (!numbers.startsWith('//')) {
+        return { delimiters: defaultDelimiters, numberString: numbers };
     }
 
+    const [delimiterPart, ...numberParts] = numbers.split('\n');
+    const delimiterRegex = /\[(.*?)\]/g;
+    const delimiters = [];
+    let match;
+
+    while ((match = delimiterRegex.exec(delimiterPart)) !== null) {
+        delimiters.push(match[1]);
+    }
+
+    const numberString = numberParts.join('\n');
+    if (delimiters.length === 0) {
+        delimiters.push(';');
+    }
+    return {
+        delimiters: delimiters.length ? delimiters : defaultDelimiters,
+        numberString
+    };
+}
+
+function extractNumbers(numberString, delimiters) {
     const regex = new RegExp(`[\\n${delimiters.join('')}]`);
-    const numArray = numbers.split(regex).map(Number);
-    const negatives = numArray.filter(num => num < 0);
+    return numberString.split(regex).map(Number);
+}
+
+function validateNegatives(numbers) {
+    const negatives = numbers.filter(num => num < 0);
     if (negatives.length > 0) {
         throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
     }
-    return numArray.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
 }
+
+function sumNumbers(numbers) {
+    return numbers.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
+}
+
 module.exports = add;
