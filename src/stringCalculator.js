b3e041a9d3a33d2abd7bd2b9a4f3e6ffbe068525
sachinpsgg <sachin@psglobalgroup.com>
2025-06-12 19:01:27 +0530

    test 6 for handeling negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,6 +10,10 @@ function add(numbers) {
     }
 
     const numArray = numbers.split(new RegExp(`[\\n${delimiter}]`)).map(Number);
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
+    }
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 module.exports = add;
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -25,4 +25,9 @@ describe('add function', () => {
     it('should support custom delimiters', () => {
         expect(add("//;\n1;2")).toBe(3);
     });
+
+    //test 6
+    it('should throw an error for negative numbers', () => {
+        expect(() => add("1,-2,3")).toThrow("negatives not allowed: -2");
+    });
 });
