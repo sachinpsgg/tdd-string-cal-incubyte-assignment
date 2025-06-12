d1dae36f3b2542324292fc36e419d9c9652d2857
sachinpsgg <sachin@psglobalgroup.com>
2025-06-12 18:31:35 +0530

    test 2 for single number string

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,7 +1,13 @@
 const add = require('../src/stringCalculator');
 
 describe('add function', () => {
+    // test 1
     it('should return 0 for an empty string', () => {
         expect(add("")).toBe(0);
+    });
+
+    //test 2
+    it('should return number for single number', () => {
+        expect(add("1")).toBe(1);
     });
 });
