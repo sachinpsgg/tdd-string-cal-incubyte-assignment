1a5852c60845302828849d26c9c92b11e24c5e47
sachinpsgg <sachin@psglobalgroup.com>
2025-06-12 18:30:24 +0530

    using describe in place of test

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,5 +1,7 @@
 const add = require('../src/stringCalculator');
 
-test('should return 0 for an empty string', () => {
-    expect(add("")).toBe(0);
+describe('add function', () => {
+    it('should return 0 for an empty string', () => {
+        expect(add("")).toBe(0);
+    });
 });
